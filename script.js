cd527a99405d6dddfba04c80f456a1e615a51956
Ye-Dam Kim <ydk222@nyu.edu>
2024-06-21 12:17:33 -0400

    changed sort names

--- script.js
+++ script.js
@@ -137,8 +137,8 @@ function displaySortedFollowers(followers, sortOption) {
     sortOptions.innerHTML = `
         <option value="timestamp-desc">Newest</option>
         <option value="timestamp-asc">Oldest</option>
-        <option value="value-asc">Alphabetical(0-9, _, A-Z)</option>
-        <option value="value-desc">Alphabetical(Z-A, _, 9-0)</option>
+        <option value="value-asc">0-9, _, a-z</option>
+        <option value="value-desc">z-a, _, 9-0</option>
     `;
 
     sortOptions.value = sortOption; // Preserve selected option
