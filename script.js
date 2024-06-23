552704b08d44e7cdbdd6c21c0898478f3e9b970a
Ye-Dam Kim <ydk222@nyu.edu>
2024-06-23 00:41:43 -0400

    implemented following

--- script.js
+++ script.js
@@ -13,21 +13,22 @@ async function analyzeFolder() {
     for (let file of files) {
         if (file.name === 'followers_1.json') {
             userData.followers = JSON.parse(await file.text());
+        } else if (file.name === 'following.json') {
+            const followingData = JSON.parse(await file.text());
+            userData.following = followingData.relationships_following;
         }
-        // Add other conditions for different files if needed
-        // Example:
-        // else if (file.name === 'blocked_accounts.json') {
-        //     userData.blockedAccounts = JSON.parse(await file.text());
-        // }
     }
 
     if (Object.keys(userData).length > 0) {
         // Store the entire userData object in local storage
         localStorage.setItem('userData', JSON.stringify(userData));
 
-        // Display the followers link if followers data is present
+        // Display the links if data is present
         if (userData.followers) {
             document.getElementById('followersLink').style.display = 'block';
+        }
+        if (userData.following) {
+            document.getElementById('followingLink').style.display = 'block';
         }
     } else {
         alert('No specific files found.');
@@ -39,7 +40,12 @@ document.getElementById('folderInput').addEventListener('change', analyzeFolder)
 // Check local storage on page load
 document.addEventListener('DOMContentLoaded', () => {
     const userData = JSON.parse(localStorage.getItem('userData'));
-    if (userData && userData.followers) {
-        document.getElementById('followersLink').style.display = 'block';
+    if (userData) {
+        if (userData.followers) {
+            document.getElementById('followersLink').style.display = 'block';
+        }
+        if (userData.following) {
+            document.getElementById('followingLink').style.display = 'block';
+        }
     }
 });
